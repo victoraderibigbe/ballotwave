b9b99471f8a32dbff3a4a7913a7ac24b58355f1b
Adeshola Ayomide <josephay125d@gmail.com>
2024-06-29 07:45:07 -0700

    voting page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
