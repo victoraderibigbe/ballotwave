b17efda0572b0dbf550353e5644474799bc66681
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-07-01 12:25:16 +0100

    candidates commit

--- src/components/DashNav.jsx
+++ src/components/DashNav.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const DashNav = () => {
   return (
     <>
-      <nav class="navbar bg-body-tertiary px-0 shadow fixed-top">
-        <div class="container-fluid">
+      <nav className="navbar bg-body-tertiary px-0 shadow fixed-top">
+        <div className="container-fluid">
           <div>
             <img
               src="https://img.freepik.com/premium-vector/modern-unique-hexagon-letter-bw-logo-design-template-elegant-initial-bw-letter-logo-concept_1101554-432.jpg?w=740"
@@ -22,52 +22,58 @@ const DashNav = () => {
             data-bs-target="#offcanvasNavbar"
             aria-controls="offcanvasNavbar"
             aria-label="Toggle navigation"
-            class="navbar-toggler-icon"
+            className="navbar-toggler-icon"
           ></span>
 
           <div
-            class="offcanvas offcanvas-end"
+            className="offcanvas offcanvas-end"
             tabindex="-1"
             id="offcanvasNavbar"
             aria-labelledby="offcanvasNavbarLabel"
           >
-            <div class="offcanvas-header">
+            <div className="offcanvas-header">
               <p className="navbar-brand fw-bold fs-5">BallotWave</p>
               <button
                 type="button"
-                class="btn-close"
+                className="btn-close"
                 data-bs-dismiss="offcanvas"
                 aria-label="Close"
               ></button>
             </div>
-            <div style={{ marginTop: "-40px" }} class=" offcanvas-body">
-              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item">
-                  <Link class="nav-link" aria-current="page" to="/">
+            <div style={{ marginTop: "-40px" }} className=" offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/">
                     Home
                     <hr />
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link class="nav-link" to="/dashboard">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/dashboard">
                     Vote Now
                     <hr />
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link class="nav-link" to="/voting/results">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/voting/results">
                     Voting Result
                     <hr />
                   </Link>
                 </li>
-<li class="nav-item">
-                  <Link class="nav-link" to="/profilepage">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/candidate">
+                    Candidates
+                    <hr />
+                  </Link>
+                </li>
+<li className="nav-item">
+                  <Link className="nav-link" to="/profilepage">
                     Profile
                     <hr />
                   </Link>
                 </li>
-                <li class="nav-item fixed-bottom text-end px-3 ">
-                  <Link class="text-danger nav-link" href="#">
+                <li className="nav-item fixed-bottom text-end px-3 ">
+                  <Link className="text-danger nav-link" href="#">
                     Log-Out
                   </Link>
                 </li>
